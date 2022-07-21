162cbd89c411a02965fa0b1349f3b7626f1e5eb7
eugenekim <eugenekim3107@gmail.com>
2022-07-21 10:29:19 -0700

    finished initial neural network

--- mlp/activations.py
+++ mlp/activations.py
@@ -6,3 +6,9 @@ def tanh(x):
 
 def tanh_prime(x):
     return 1 - np.tanh(x)**2
+
+def ReLU(x):
+    return np.maximum(x, 0)
+
+def ReLU_prime(x):
+    return x > 0
---
+++ mlp/classifierMNIST.py
@@ -0,0 +1,7 @@
+from Layer import Layer
+from activationLayer import activationLayer
+from classifierNN import classifierNN
+from activations import ReLU, ReLU_prime
+from losses import cross_entropy, cross_entropy_prime
+import numpy as np
+
