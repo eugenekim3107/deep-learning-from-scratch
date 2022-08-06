c516622d203d2ffb0263d5bdc2069f589db87633
eugenekim <eugenekim3107@gmail.com>
2022-08-05 23:42:28 -0700

    finished initial neural network

--- mlp/mlpMNIST.py
+++ mlp/mlpMNIST.py
@@ -1,15 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from Layer import Layer
 from activationLayer import activationLayer
 from NN import NN
 from activations import ReLU, ReLU_prime, tanh, tanh_prime
 from losses import mse, mse_prime
-import numpy as np
-import pandas as pd
-import os
 import torch
+import os
+import pandas as pd
 
-# data file
 os.chdir("..")
+
+# prepare data
 data = pd.read_csv("train.csv")
 x_train = data.loc[:,"pixel0":].to_numpy().reshape(-1,1,784)
 x_train = x_train.astype(np.float32)
@@ -18,23 +20,8 @@ temp = data.loc[:,"label"].to_numpy()
 y_train = np.zeros((temp.shape[0], 10, 1))
 for num in range(len(temp)):
     y_train[num][temp[num]] = 1
-y_train = y_train.reshape(-1,1,10)
+y_train = y_train.reshape(-1, 1, 10)
 
-# network
-net = NN()
-net.add(Layer(784, 100))
-net.add(activationLayer(ReLU, ReLU_prime))
-net.add(Layer(100,10))
-net.add(activationLayer(tanh, tanh_prime))
-
-# train
-net.use(mse, mse_prime)
-net.fit(x_train[0:1000], y_train[0:1000], epochs=100, learning_rate=0.05)
-
-# test
+net = torch.load("mlp.obj")
 out = net.predict(x_train)
-print(out[0])
-print(y_train[0])
 
-# save neural network
-torch.save(net, "mlp.obj")
