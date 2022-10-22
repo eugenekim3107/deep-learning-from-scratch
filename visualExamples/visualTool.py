026acccc73e833c05b8596bf63597831bf33dbf0
eugenekim <eugenekim3107@gmail.com>
2022-10-23 02:40:09 +0900

    finished initial neural network

--- visualExamples/visualTool.py
+++ visualExamples/visualTool.py
@@ -2,14 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def toolMNIST(x_train, y_train, y_pred):
-    random = np.random.randint(0, x_train.shape[0])
-    y_train =  y_train[random][0]
-    y_pred = y_pred[random][0]
-    max_val = y_pred[0]
-    max_idx = 0
-    true_val = y_train.argmax(axis=0)
-    xreshaped = x_train[random].reshape(28,28)
-    print("Predicted Value: " + str(max_idx) + "\n" + "True Value: " + str(true_val))
-    plt.imshow(xreshaped, cmap="gray")
-    plt.show()
-    plt.close()
+    fig = plt.figure()
+    y_pred = np.argmax(np.array(y_pred), axis=2)
+    for i in range(9):
+
+        # get data
+        random = np.random.randint(0, y_train.shape[0])
+        y_train_temp = np.argmax(y_train[random])
+        y_pred_temp = y_pred[random]
+        xreshaped = x_train[random].reshape(28, 28)
+
+        subplot = fig.add_subplot(3, 3, i+1)
+        subplot.set_xticks([])
+        subplot.set_yticks([])
+        subplot.set_title(f"Pred:{int(y_pred_temp)}, Label:{y_train_temp}")
+        subplot.imshow(xreshaped, cmap = plt.cm.gray_r)
+    plt.savefig("visualExamples/samplePredictionsMNIST.png")
